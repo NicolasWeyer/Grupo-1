b90d0dd8f60f9bd72326b5a5a772e9c52d4a5715
NicolasWeyer <nicolaseweyer@gmail.com>
2024-08-20 19:01:05 -0300

    Cálculo de complejidad asintótica 1a

--- clase1/paq/ej1.java
+++ clase1/paq/ej1.java
@@ -6,20 +6,26 @@ public class ej1 {
         
         //Punto a
 
-        int max = 0;
-        int [] arr = new int [10];
-        arr[0] = 12;
-        arr[1] = 9;
-        arr[2] = 1;
-        arr[3] = 4;
+        int max = 0; //1
+        int [] arr = new int [10]; //1
+        arr[0] = 12; //1
+        arr[1] = 9; //1
+        arr[2] = 1; //1
+        arr[3] = 4; //1
 
-        for(int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) 
-                max = arr[i];
+        for(int i = 0; i < arr.length; i++) { //1 + 2(n + 1) + n = 3n + 3
+            if (arr[i] > max) //2n
+                max = arr[i]; //2
         }
 
         System.out.println(max);
 
+        //aprox = 5n + 11 (el if es n por el for, no porque el comando if sea un ciclo)
+        //5n + 11 <= cn, n = 5, c = 12
+        // 5/5 + 11 = 12 <= 12, g(n) pertenece a O(n) ---> orden lineal
+
+
+        
 
 
     }
