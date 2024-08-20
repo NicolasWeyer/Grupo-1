ea3376603d853f05e91d9cd0ad696a0d4b5ef648
NicolasWeyer <nicolaseweyer@gmail.com>
2024-08-20 20:21:33 +0000

    prueba ejercicio 1a clase 1

--- clase1/paq/ej1.java
+++ clase1/paq/ej1.java
@@ -4,7 +4,23 @@ public class ej1 {
     
     public static void main(String[] args) {
         
-        System.out.println("hola");
+        //Punto a
+
+        int max = 0;
+        int [] arr = new int [10];
+        arr[0] = 12;
+        arr[1] = 9;
+        arr[2] = 1;
+        arr[3] = 4;
+
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) 
+                max = arr[i];
+        }
+
+        System.out.println(max);
+
+
 
     }
 
---
+++ clase2/ejercicios/ej1.java
@@ -0,0 +1,5 @@
+package clase2.ejercicios;
+
+public class ej1 {
+    
+}
