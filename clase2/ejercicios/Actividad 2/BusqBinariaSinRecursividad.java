315952850373e8755253b06f7d6c43848d35ef8a
FacuPlanas <facuplanas2@gmail.com>
2024-08-26 17:56:33 -0300

    Final Actividad 2

--- clase2/ejercicios/Actividad 2/BusqBinariaSinRecursividad.java
+++ clase2/ejercicios/Actividad 2/BusqBinariaSinRecursividad.java
@@ -18,4 +18,21 @@ public class BusquedaBinaria {
         return -1;
     }
 
+// Utilizando el siguiente main con el arreglo: {22, 31, 5, 47, 19, 11, 100}
+    public static void main(String[] args) {
+
+        int[] arreglo = {22, 31, 5, 47, 19, 11, 100};
+        int objetivo = 7;
+        int resultado = busquedaBinaria(arreglo, objetivo);
+
+        if (resultado == -1) {
+            System.out.println("Elemento no encontrado en el arreglo.");
+        } else {
+            System.out.println("Elemento encontrado en el índice: " + resultado);
+        }
+    }
+// Si el numero que busco es el 19 el resultado por consola es: Elemento encontrado en el indice: 4
+
+// Si el numero que busco es el 10 el resultado por consola es: Elemento no encontrado en el arreglo
+
 }
