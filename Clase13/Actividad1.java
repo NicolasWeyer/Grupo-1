6da3ed78bc497f725f4a333b7a43e4cf36bdce05
FacuPlanas <facuplanas2@gmail.com>
2024-11-17 16:15:37 -0300

    Clase 13 actividad 1 incompleta

---
+++ Clase13/Actividad1.java
@@ -0,0 +1,14 @@
+
+1)  El atacante podría intentar realizar las siguientes acciones maliciosas:
+
+   1- Inyección de SQL: Manipular consultas en la base de datos para obtener información sensible.
+   2- Fuerza bruta: Intentar adivinar credenciales de acceso mediante múltiples combinaciones.
+   3- Ataque de denegación de servicio (DDoS): Sobrecargar los recursos del servidor para dejar la plataforma fuera de servicio.
+
+2)  Funciones del sistema:
+
+   1- Monitorización de patrones de acceso: Identificar solicitudes sospechosas, como múltiples intentos de login fallidos en poco tiempo.
+   2- Análisis del tráfico en tiempo real: Detectar anomalías como grandes cantidades de solicitudes similares.
+   3- Validación de entradas del usuario: Filtrar entradas sospechosas para evitar inyecciones de código.
+   4- Bloqueo de direcciones IP sospechosas: Usar firewalls para evitar acceso de atacantes.
+   5- Escaneo de archivos subidos: Analizar documentos en busca de malware.
